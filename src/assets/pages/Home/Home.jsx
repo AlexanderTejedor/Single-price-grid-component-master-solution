de093fa05490f8796c44fc784e010c1224457f0b
Alexander Tejedor <atromero.98@gmail.com>
2025-04-27 08:59:13 -0500

    feat: add initial JSX layout for main page structure

---
+++ src/assets/pages/Home/Home.jsx
@@ -0,0 +1,34 @@
+const Home = () => {
+    return (
+        <section className=''>
+            <section>
+                <h1>Join our community</h1>
+                <h2>30-day, hassle-free money back guarantee</h2>
+                <p>Gain access to our full library of tutorials along with expert code reviews.
+                Perfect for any developers who are serious about honing their skills.</p>
+            </section>
+            <section>
+                <div>
+                    <h3>Monthly Subscription</h3>
+                    <p><span>$29 </span>per month</p>
+                    <p>Full access for less than $1 a day</p>
+                    <button>Sign Up</button>
+                </div>
+                <div>
+                    <h3>Why Us</h3>
+                    <ul>
+                        <li>Tutorials by industry experts</li>
+                        <li>Peer & expert code review</li>
+                        <li>Coding exercises</li>
+                        <li>Access to our GitHub repos</li>
+                        <li>Community forum</li>
+                        <li>Flashcard decks</li>
+                        <li>New videos every week</li>
+                    </ul>
+                </div>
+            </section>
+        </section>
+    )
+}
+
+export { Home };
